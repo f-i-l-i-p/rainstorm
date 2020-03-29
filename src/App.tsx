f99f5dac9136164581087df0c8d2b84f4a7a20ff
Filip Johnsson <Filip_Johnsson@yahoo.se>
2020-03-29 11:22:17 +0200

    Moved antd.css import to App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import 'antd/dist/antd.css';
 import SearchView from './geocode/components/SearchView';
 import WeatherView from './weather/components/WeatherView';
 import WeatherPage from './weather/components/WeatherPage';
