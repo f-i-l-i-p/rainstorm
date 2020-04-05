70d8b4f1553cad882b60651be94dece560d4a8d6
Filip Johnsson <Filip_Johnsson@yahoo.se>
2020-04-05 10:00:21 +0200

    Lifted WeatherView props to WeatherPage

--- src/weather/components/WeatherPage.tsx
+++ src/weather/components/WeatherPage.tsx
@@ -1,6 +1,8 @@
 import React, { StrictMode, Fragment } from "react";
 import { ILocation } from "../../geocode/types";
 import WeatherView from "./WeatherView";
+import { IForecast } from "../types";
+import collectWeatherData from "../search";
 
 
 interface IWeatherPageProps {
@@ -9,6 +11,8 @@ interface IWeatherPageProps {
 
 interface IWeatherPageState {
     location: ILocation,
+    times: Date[],
+    forecasts: IForecast[],
 }
 
 class WeatherPage extends React.Component<IWeatherPageProps, IWeatherPageState>{
@@ -23,14 +27,46 @@ class WeatherPage extends React.Component<IWeatherPageProps, IWeatherPageState>{
                 long: 16.545025,
                 alt: 0,
             },
+            times: getTimes(5, 1000 * 60 * 60),
+            forecasts: [],
         }
+    }
+
+    componentDidMount() {
+        this.updateWeather(this.state.location.lat.toString(), this.state.location.long.toString())
+    }
+
+    private async updateWeather(lat: string, long: string) {
+        const weather = await collectWeatherData(lat, long);
+
+        this.setState({
+            ...this.state,
+            forecasts: weather,
+        });
     }
 
     render() {
         return (
-            <WeatherView location={this.state.location} />
+            <WeatherView location={this.state.location} times={this.state.times} forecasts={this.state.forecasts} />
         );
     }
+}
+
+// Returns a list of Date objects with a specified time difference
+function getTimes(count: number, interval: number): Date[] {
+    let times: Date[] = []
+
+    let start = new Date(Date.now())
+    start.setHours(start.getHours() + 1)
+    start.setMinutes(0);
+    start.setSeconds(0);
+    start.setMilliseconds(0);
+
+    for (let i = 0; i < count; i++) {
+        times.push(new Date(start.getTime() + (i * interval)));
+    }
+
+    return times;
 }
 
 export default WeatherPage;
--- src/weather/components/WeatherView.tsx
+++ src/weather/components/WeatherView.tsx
@@ -6,59 +6,21 @@ import { ILocation } from "../../geocode/types";
 
 interface IWeatherViewProps {
     location: ILocation,
-}
-
-interface IWeatherViewState {
     times: Date[],
     forecasts: IForecast[],
 }
 
+interface IWeatherViewState {
+}
+
 class WeatherView extends React.Component<IWeatherViewProps, IWeatherViewState> {
-    constructor(props: Readonly<IWeatherViewProps>) {
-        super(props);
-
-        this.state = {
-            times: this.getTimes(5, 1000 * 60 * 60),
-            forecasts: [],
-        }
-    }
-
-    componentDidMount() {
-        this.updateWeather(this.props.location.lat.toString(), this.props.location.long.toString())
-    }
-
-    private getTimes(count: number, interval: number): Date[] {
-        let times: Date[] = []
-
-        let start = new Date(Date.now())
-        start.setHours(start.getHours() + 1)
-        start.setMinutes(0);
-        start.setSeconds(0);
-        start.setMilliseconds(0);
-
-        for (let i = 0; i < count; i++) {
-            times.push(new Date(start.getTime() + (i * interval)));
-        }
-
-        return times;
-    }
-
-    private async updateWeather(lat: string, long: string) {
-        const weather = await collectWeatherData(lat, long);
-
-        this.setState({
-            ...this.state,
-            forecasts: weather,
-        });
-    }
-
-    public render() {
+      public render() {
         return (
             <div style={{
                 display: "flex",
                 flexDirection: "row",
             }}>
-                <WeatherTable targetTimes={this.state.times} forecasts={this.state.forecasts}/>
+                <WeatherTable targetTimes={this.props.times} forecasts={this.props.forecasts}/>
             </ div>
         );
     }
