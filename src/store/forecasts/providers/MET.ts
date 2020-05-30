3fb89d78f212048961bd24180e905dc35d2e12ea
Filip Johnsson <Filip_Johnsson@yahoo.se>
2020-05-30 18:07:29 +0200

    Added MET icons

--- src/store/forecasts/providers/MET.ts
+++ src/store/forecasts/providers/MET.ts
@@ -1,9 +1,22 @@
-import { IForecast, IWeatherProvider, ITimePoint } from "../../types";
+import { IForecast, IWeatherProvider, ITimePoint, IWeather } from "../../types";
 import logo from '../../../icons/provider logos/MET.jpg'
 
 export const METWeatherProvider: IWeatherProvider = {
     name: 'MET',
     logo: logo
+}
+
+function getIcon(icon: number): string {
+    switch (icon) {
+        case 1:
+            return "clear_sky";
+        case 2:
+            return "nearly_clear_sky";
+        case 3:
+            return "halfclear_sky";
+        default:
+            return "unknown";
+    }
 }
 
 // Fetches weather data from MET
@@ -41,24 +54,33 @@ function METToITimePoints(json: any): ITimePoint[] {
     try {
         const times = json['weatherdata']['product'][0]['time'];
 
-        times.forEach((timeObject: any) => {
-            const from = timeObject['$']['from']
-            const to = timeObject['$']['to']
+        for (let i = 0; i < times.length - 1; i += 1) {
+            const timeObjectA = times[i];
+            const timeObjectB = times[i + 1];
 
-            if (from === to){
-                const data = timeObject['location'][0];
+            const fromA = new Date(timeObjectA['$']['from']);
+            const toA = new Date(timeObjectA['$']['to']);
+            const fromB = new Date(timeObjectB['$']['from']);
+            const toB = new Date(timeObjectB['$']['to']);
+
+            if (fromA.getTime() === toA.getTime() &&
+                fromA.getTime() === toB.getTime() &&
+                toB.getTime() === fromB.getTime() + 1000 * 60 * 60) {
+
+                const dataA = timeObjectA['location'][0];
+                const dataB = timeObjectB['location'][0];
 
                 timePoints.push({
-                    time: new Date(from),
+                    time: fromA,
                     weather: {
-                        temperature: data['temperature'][0]['$']['value'],
-                        wind: data['windSpeed'][0]['$']['mps'],
-                        gust: data['windGust'][0]['$']['mps'],
-                        symbol: ''
+                        temperature: dataA['temperature'][0]['$']['value'],
+                        wind: dataA['windSpeed'][0]['$']['mps'],
+                        gust: dataA['windGust'][0]['$']['mps'],
+                        symbol: getIcon(Number(dataB['symbol'][0]['$']['number'])),
                     }
                 });
             }
-        });
+        }
     }
     catch (error) {
         console.error(error.message)
