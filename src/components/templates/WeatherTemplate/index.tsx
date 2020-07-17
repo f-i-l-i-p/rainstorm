2cc316d5fcd561a52f546b1a1071043187ba73d7
Filip Johnsson <Filip_Johnsson@yahoo.se>
2020-07-17 21:08:48 +0200

    Removed current weather & small UI changes

--- src/components/templates/WeatherTemplate/index.tsx
+++ src/components/templates/WeatherTemplate/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { IForecast, IWeather } from "../../../store/types";
+import { IForecast } from "../../../store/types";
 import { ILocation } from "../../../store/types";
-import CurrentWeather from "../../molecules/CurrentWeather";
 import WeatherTable from "../../organisms/WeatherTable";
 import ToggleButton from "../../atoms/ToggleButton";
+import { Typography } from "antd";
 
 import './style.css';
+
+const { Title } = Typography;
 
 interface IDisplayMode {
     title: string,
@@ -21,39 +23,19 @@ interface IWeatherViewProps {
 
 const WeatherView = (props: IWeatherViewProps) => (
     <div id="weather-template-container">
-        {props.location &&
-            <CurrentWeather location={props.location} weather={currentWeather(props.displayForecasts)} />
-        }
-        <ToggleButton
-            className="weather-time-button"
-            type="primary"
-            shape="round"
-            options={props.displayModes.map(mode => { return { title: mode.title, onClick: mode.activate } })}
-        />
+        <div className="row">
+            {props.location &&
+                <Title level={2}>{props.location.name}</Title>
+            }
+            <ToggleButton
+                className="row-end-item"
+                type="primary"
+                shape="round"
+                options={props.displayModes.map(mode => { return { title: mode.title, onClick: mode.activate } })}
+            />
+        </div>
         <WeatherTable displayTimes={props.displayTimes} displayForecasts={props.displayForecasts} />
     </ div>
 )
-
-// Returns the average temperature from the first temperature of all forecasts
-// TODO: move to store
-function currentWeather(forecasts: IForecast[]): IWeather {
-    let totalTemp = 0;
-    let totalData = 0;
-    forecasts.forEach(forecast => {
-        if (forecast.times.length > 0) {
-            // So apparently the temperature think that it is a string or something and this
-            // wont work without converting it to a number.
-            totalTemp = totalTemp + +forecast.times[0].weather.temperature;
-            totalData++;
-        }
-    });
-    // TODO: return average weather
-    return {
-        temperature: totalTemp / totalData,
-        wind: NaN,
-        gust: NaN,
-        symbol: '',
-    }
-}
 
 export default WeatherView;
