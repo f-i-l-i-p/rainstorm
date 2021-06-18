3f67fcfc92ee947bfe59a4f405efd99224e76e46
Filip Johnsson <filip_johnsson@yahoo.se>
2021-06-18 11:47:26 +0200

    Remove logo

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from '../../icons/temp.png';
 
 interface IHeaderProps {
     items?: JSX.Element[],
@@ -12,12 +11,10 @@ interface IHeaderState {
 export default class Header extends React.Component<IHeaderProps, IHeaderState>{
     render() {
         return (
-            <div className="header" style={{ display: "flex", flexDirection: "row", alignItems: "center", padding: "5px 15px"}}>
-                
-                <img src={logo} height='60ch'/>
-                
+            <div className="header" style={{ display: "flex", flexDirection: "row", alignItems: "center", padding: "5px 15px" }}>
+
                 {this.props.items &&
-                    <div style={{ paddingLeft: "20px", display: "flex", flexDirection: "row", alignItems: "center"}}>
+                    <div style={{ paddingLeft: "20px", display: "flex", flexDirection: "row", alignItems: "center" }}>
                         {this.props.items.map((item, index) => (
                             <React.Fragment key={index}>
                                 {item}
