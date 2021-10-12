47fd449d306365a8f57efbc15a3927dda2fe5510
Filip Johnsson <filip_johnsson@yahoo.se>
2021-10-12 13:21:49 +0200

    Table names

--- src/components/weather/WeatherTable/index.tsx
+++ src/components/weather/WeatherTable/index.tsx
@@ -9,68 +9,72 @@ import { ITableData } from "./types";
 
 interface IWeatherTableListProps {
     tableData: ITableData,
+    name: string,
 }
 
 class WeatherTable extends React.Component<IWeatherTableListProps> {
     public render() {
         return (
-            <div className="columns">
+            <div>
+                {this.props.name}
+                <div className="columns">
 
-                {/* Time row background */}
-                <div className="right-column">
-                    <div className="time-row" style={{ width: '100%' }}>
-                        <Paper style={{ width: '100%', height: '100%' }} />
-                    </div>
-                </div>
-
-                {/* Weather row background. One for each provider. */}
-                <div className="all-columns">
-                    <div className="time-row" />
-                    {this.props.tableData.providers.map((ignored, index) =>
-                        <div key={index} className="weather-row">
+                    {/* Time row background */}
+                    <div className="right-column">
+                        <div className="time-row" style={{ width: '100%' }}>
                             <Paper style={{ width: '100%', height: '100%' }} />
                         </div>
-                    )}
-                </div>
+                    </div>
 
-                {/* Provider Name */}
-                <div className="left-column">
-                    <div className="time-row" />
-                    {this.props.tableData.providers.map((provider, index) =>
-                        <div key={index} className="weather-row">
-                            <Typography className="weather-provider-name">
-                                {provider.name}
-                            </Typography>
-                        </div>
-                    )}
-                </div>
-
-                {/* Divider */}
-                <div className="divider-column">
-                    <div className="time-row" />
-                    {this.props.tableData.providers.map((ignored, index) =>
-                        <div key={index} className="weather-row">
-                            <Divider type="vertical" style={{ height: 'calc(100% - 5px)', margin: 0 }} />
-                        </div>
-                    )}
-                </div>
-
-                {/* Time cells and Weather cells */}
-                <div className="right-column" style={{ overflowX: 'auto' }}>
-                    <div className="time-row">
-                        {Array.from(this.props.tableData.columns, ([timeMillis, column]) =>
-                            <TimeCell  key={timeMillis} time={column.date} />
+                    {/* Weather row background. One for each provider. */}
+                    <div className="all-columns">
+                        <div className="time-row" />
+                        {this.props.tableData.providers.map((ignored, index) =>
+                            <div key={index} className="weather-row">
+                                <Paper style={{ width: '100%', height: '100%' }} />
+                            </div>
                         )}
                     </div>
-                    {this.props.tableData.providers.map((provider, index) =>
-                        <div key={index} className="weather-row">
+
+                    {/* Provider Name */}
+                    <div className="left-column">
+                        <div className="time-row" />
+                        {this.props.tableData.providers.map((provider, index) =>
+                            <div key={index} className="weather-row">
+                                <Typography className="weather-provider-name">
+                                    {provider.name}
+                                </Typography>
+                            </div>
+                        )}
+                    </div>
+
+                    {/* Divider */}
+                    <div className="divider-column">
+                        <div className="time-row" />
+                        {this.props.tableData.providers.map((ignored, index) =>
+                            <div key={index} className="weather-row">
+                                <Divider type="vertical" style={{ height: 'calc(100% - 5px)', margin: 0 }} />
+                            </div>
+                        )}
+                    </div>
+
+                    {/* Time cells and Weather cells */}
+                    <div className="right-column" style={{ overflowX: 'auto' }}>
+                        <div className="time-row">
                             {Array.from(this.props.tableData.columns, ([timeMillis, column]) =>
-                                <WeatherCell key={timeMillis} weather={column.weatherMap.get(provider) as IWeather} />
+                                <TimeCell key={timeMillis} time={column.date} />
                             )}
                         </div>
-                    )}
-                </div>
-            </div >
+                        {this.props.tableData.providers.map((provider, index) =>
+                            <div key={index} className="weather-row">
+                                {Array.from(this.props.tableData.columns, ([timeMillis, column]) =>
+                                    <WeatherCell key={timeMillis} weather={column.weatherMap.get(provider) as IWeather} />
+                                )}
+                            </div>
+                        )}
+                    </div>
+                </div >
+            </div>
         );
     }
 }
--- src/components/weather/WeatherTableList/index.tsx
+++ src/components/weather/WeatherTableList/index.tsx
@@ -6,12 +6,23 @@ import { getDayOffset, listHoursFromNow } from "../../../helpers/date";
 import { ITableData } from "../WeatherTable/types";
 import { table } from "console";
 import { IWeather } from "../../../weather/types";
+import { resolveSoa } from "dns/promises";
 
 interface IWeatherTableListProps {
     weatherStateForecasts: IWeatherStateForecast[],
 }
 
 export default class WeatherTableList extends React.Component<IWeatherTableListProps> {
+
+    private days = {
+        1: "Måndag",
+        2: "Tisdag",
+        3: "Onsdag",
+        4: "Torsdag",
+        5: "Fredag",
+        6: "Lördag",
+        7: "Söndag",
+    }
 
     /**
      * Converts the data from props to data accepted by the weather tables.
@@ -33,7 +44,7 @@ export default class WeatherTableList extends React.Component<IWeatherTableListP
         stateForecasts.forEach(stateForecast => {
             if (!stateForecast.loading) {
                 stateForecast.forecast.weatherPoints.forEach((weatherTime) => {
-                    
+
                     // Sometimes the date is a string because the reducer is bad.
                     let date: Date;
                     if (typeof weatherTime.time === 'string')
@@ -81,13 +92,53 @@ export default class WeatherTableList extends React.Component<IWeatherTableListP
         return 24;
     }
 
+    private getTableName(index: number, date: Date | undefined): string {
+        let result = "";
+
+        if (!date) return result;
+
+        switch (index) {
+            case 0:
+                result += "Idag, ";
+                break;
+            case 1:
+                result += "Imorgon, "
+        }
+
+        switch (date.getDay()) {
+            case 1:
+                result += "Måndag";
+                break;
+            case 2:
+                result += "Tisdag";
+                break;
+            case 3:
+                result += "Onsdag";
+                break;
+            case 4:
+                result += "Torsdag";
+                break;
+            case 5:
+                result += "Fredag";
+                break;
+            case 6:
+                result += "Lördag";
+                break;
+            case 0:
+                result += "Söndag";
+                break;
+        }
+
+        return result;
+    }
+
     public render() {
         let tableData = this.getTableData();
 
         return (
             <div>
                 {tableData.map((data, index) =>
-                    <WeatherTable key={index} tableData={data} />
+                    <WeatherTable key={index} tableData={data} name={this.getTableName(index, data.columns.get(data.columns.keys().next().value)?.date)} />
                 )}
             </div>
         );
