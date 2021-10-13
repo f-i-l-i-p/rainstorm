27f67d9f16612c07d7d9039e75282b192f4dab32
Filip Johnsson <filip_johnsson@yahoo.se>
2021-10-13 18:35:53 +0200

    Remove smhi2

--- src/weather/index.ts
+++ src/weather/index.ts
@@ -3,9 +3,8 @@ import { ILocation } from "../store/types";
 import { IForecast, IWeatherProvider } from "./types";
 import MET from "./weatherProviders/MET";
 import SMHI from "./weatherProviders/SMHI";
-import SMHI2 from "./weatherProviders/SMHI2";
 
-const weatherProviders: IWeatherProvider[] = [new SMHI(), new MET(), new SMHI2()]
+const weatherProviders: IWeatherProvider[] = [new SMHI(), new MET()]
 
 
 export function getWeatherProviders(): IWeatherProvider[] {
