93fca45397bd3c4a9102f02ce7e4fffa18a611f0
Filip Johnsson <filip_johnsson@yahoo.se>
2021-11-12 18:33:53 +0100

    UI fixes

--- src/components/location/LocationSearch/index.tsx
+++ src/components/location/LocationSearch/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Spin } from "antd";
+import { Button, Divider, Input, Spin } from "antd";
 import React from "react";
 import { connect } from "react-redux";
 import { SearchOutlined } from '@ant-design/icons';
@@ -22,9 +22,10 @@ class LocationSearch extends React.Component<ILocationSearchProps>{
         return (
             <div className="search">
                 <div className="search-bar">
-                    <Input size="large" autoFocus allowClear prefix={<SearchOutlined />} onChange={value => this.onChange(value.target.value)} />
-                    <Button size="large" onClick={() => this.props.close()}>Avbryt</Button>
+                    <Input className="search-input" size="large" bordered={false} placeholder="Sök" autoFocus allowClear prefix={<SearchOutlined />} onChange={value => this.onChange(value.target.value)} />
+                    <Button className="search-cancel-button" size="large" onClick={() => this.props.close()}>Avbryt</Button>
                 </div>
+                <Divider style={{margin: 0, height: 0}} />
                 {this.props.isLoading ?
                     <Spin className="location-search-spin" />
                     :
