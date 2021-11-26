0b72c25eb6133ea037f43ee86024ba65b22122f6
Filip Johnsson <filip_johnsson@yahoo.se>
2021-11-26 17:14:18 +0100

    Privacy policy link

--- src/components/pages/SettingsPage/index.tsx
+++ src/components/pages/SettingsPage/index.tsx
@@ -7,7 +7,7 @@ import { Divider, Radio, Space, Switch, Typography } from "antd";
 import { SettingTypes, ThemeModeTypes } from "../../../store/settings/types";
 import { updateSetting as updateSettings, updateThemeMode } from "../../../store/settings/actions";
 
-const { Title, Text } = Typography;
+const { Text } = Typography;
 
 interface ISettingsPageProps {
     theme: ThemeModeTypes,
@@ -53,35 +53,39 @@ class SettingsPage extends React.Component<ISettingsPageProps>{
                 <Header title="Inställningar" backButton={() => this.props.close()} backButtonName="Klar" />
 
                 <div className="settings-content">
-                    <Text className="settings-options-title" type="secondary" strong>Utseende</Text>
-                    <Divider />
-                    <Radio.Group className="settings-options" value={themeValue} onChange={(e) => this.onThemeChange(e.target.value)}>
-                        <Space direction="vertical">
-                            <Radio value={1}>Ljust</Radio>
-                            <Divider />
-                            <Radio value={2}>Mörkt</Radio>
-                            <Divider />
-                            <Radio value={3}>Följ System</Radio>
-                        </Space>
-                    </Radio.Group>
-                    <Divider />
+                    <div>
+                        <Text className="settings-options-title" type="secondary" strong>Utseende</Text>
+                        <Divider />
+                        <Radio.Group className="settings-options" value={themeValue} onChange={(e) => this.onThemeChange(e.target.value)}>
+                            <Space direction="vertical">
+                                <Radio value={1}>Ljust</Radio>
+                                <Divider />
+                                <Radio value={2}>Mörkt</Radio>
+                                <Divider />
+                                <Radio value={3}>Följ System</Radio>
+                            </Space>
+                        </Radio.Group>
+                        <Divider />
 
-                    <Text className="settings-options-title" type="secondary" strong>Väder</Text>
-                    <Divider />
-                    <div className="settings-options">
-                        <Space direction="vertical">
-                            <div className="settings-row">
-                                <Text>Visa min/max temperatur</Text>
-                                <Switch checked={this.props.showMinMaxTemp} onChange={(status) => this.props.updateSettings({ showMinMaxTemp: status })} />
-                            </div>
-                            <Divider />
-                            <div className="settings-row">
-                                <Text>Visa byvind</Text>
-                                <Switch checked={this.props.showGust} onChange={(status) => this.props.updateSettings({ showGust: status })} />
-                            </div>
-                        </Space>
+                        <Text className="settings-options-title" type="secondary" strong>Väder</Text>
+                        <Divider />
+                        <div className="settings-options">
+                            <Space direction="vertical">
+                                <div className="settings-row">
+                                    <Text>Visa min/max temperatur</Text>
+                                    <Switch checked={this.props.showMinMaxTemp} onChange={(status) => this.props.updateSettings({ showMinMaxTemp: status })} />
+                                </div>
+                                <Divider />
+                                <div className="settings-row">
+                                    <Text>Visa byvind</Text>
+                                    <Switch checked={this.props.showGust} onChange={(status) => this.props.updateSettings({ showGust: status })} />
+                                </div>
+                            </Space>
+                        </div>
+                        <Divider />
                     </div>
-                    <Divider />
+
+                    <a id="privacy-policy" href="https://f-i-l-i-p.github.io/rainstorm/privacy-policy.html">privacy policy</a>
                 </div>
             </div>
         );
