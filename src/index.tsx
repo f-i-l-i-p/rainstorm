e71718de284be5b909443b092a7c74a096aae782
Filip Johnsson <filip_johnsson@yahoo.se>
2021-12-03 15:59:06 +0100

    Splash screen & Register service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { unregister } from './serviceWorker';
+import { register, unregister } from './serviceWorker';
 import store from "./store";
 import { Provider } from 'react-redux';
 
@@ -15,4 +15,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-unregister();
+//unregister();
+register();
