afb73fabaeee1a90e05b3b315863b006614cf70e
Filip Johnsson <filip_johnsson@yahoo.se>
2021-12-06 08:45:00 +0100

    Fix NaN weather

--- src/components/weather/WeatherCell/index.tsx
+++ src/components/weather/WeatherCell/index.tsx
@@ -18,33 +18,47 @@ const WeatherCell = (props: Props) => (
     <div className="weather_cell">
         {props.weather !== undefined &&
             <React.Fragment>
-                {props.showMinMaxTemp && props.weather.temperatureMin && props.weather.temperatureMax ?
-                    <div>
-                        <Text strong className="minimum">{Math.round(props.weather.temperatureMin)}</Text>
-                        <Text type="secondary" style={{ marginLeft: "1px", marginRight: "1px" }}>/</Text>
-                        <Text strong className="maximum">{Math.round(props.weather.temperatureMax)}</Text>
-                        <Text strong> °C</Text>
-                    </div>
+                {isNaN(props.weather.temperature) ?
+                    <Text>{" "}</Text>
                     :
-                    <Text strong>{Math.round(props.weather.temperature)} °C</Text>
+                    props.showMinMaxTemp ?
+                        <div>
+                            <Text strong className="minimum">{Math.round(props.weather.temperatureMin)}</Text>
+                            <Text type="secondary" style={{ marginLeft: "1px", marginRight: "1px" }}>/</Text>
+                            <Text strong className="maximum">{Math.round(props.weather.temperatureMax)}</Text>
+                            <Text strong> °C</Text>
+                        </div>
+                        :
+                        < Text strong> {Math.round(props.weather.temperature)} °C</Text>
                 }
                 {props.weather.symbol &&
-                    <img className='weather_symbol' alt="" /* TODO: Add alt prop! */ src={require("../../../icons3/" + props.weather.symbol + ".svg").default} />
+                    <img className='weather_symbol' alt="" src={require("../../../icons3/" + props.weather.symbol + ".svg").default} />
                 }
                 <div>
-                    <div>
-                        <Text type="secondary">{Math.round(props.weather.wind)}</Text>
-                        {props.showGust && props.weather.gust &&
-                            <Text className="maximum" type="secondary" style={{ marginLeft: "1px", marginRight: "1px" }}>{"(" + Math.round(props.weather.gust) + ")"}</Text>
-                        }
-                        <Text type="secondary">{"m/s"}</Text>
-                    </div>
-
-                    <Text type="secondary"> {props.weather.precipitation + " " + props.weather.precipitationUnit}</Text>
+                    {isNaN(props.weather.wind) || isNaN(props.weather.gust) ?
+                        <div>
+                            <Text>{" "}</Text>
+                        </div>
+                        :
+                        <div>
+                            <Text type="secondary">{Math.round(props.weather.wind)}</Text>
+                            {props.showGust && props.weather.gust &&
+                                <Text className="maximum" type="secondary" style={{ marginLeft: "1px", marginRight: "1px" }}>{"(" + Math.round(props.weather.gust) + ")"}</Text>
+                            }
+                            <Text type="secondary">{"m/s"}</Text>
+                        </div>
+                    }
+                    {isNaN(props.weather.precipitation) ?
+                        <div>
+                            <Text>{" "}</Text>
+                        </div>
+                        :
+                        <Text type="secondary"> {props.weather.precipitation + " " + props.weather.precipitationUnit}</Text>
+                    }
                 </div>
             </React.Fragment>
         }
-    </div>
+    </div >
 );
 
 function mapStateToProps(state: AppState) {
